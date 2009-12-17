e95534b8d4c59942856ce8398a81c99a7c23cb3b
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2009-12-17 01:58:25 +0000

    more copy/paste errors

--- trunk/Locales/Localization.frFR.lua
+++ trunk/Locales/Localization.frFR.lua
@@ -6,7 +6,7 @@ local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "frFR", false) -- this
 if L then
     L["About the author and SmartRes2"] = "A propos de l'auteur et de SmartRes2"
     L["All dead units are being ressed."] = "Tous les personnages morts ont \195\169t\195\169 ressuscit\195\169s."
-    -- L["And you thought the Forsaken looked bad. In about 10 seconds, %s knows %s will want a comb, some soap, and a mirror."] = "And you thought the Forsaken looked bad. In about 10 seconds, %s knows %s will want a comb, some soap, and a mirror."
+    L["And you thought the Forsaken looked bad. In about 10 seconds, %s knows %s will want a comb, some soap, and a mirror."] = "And you thought the Forsaken looked bad. In about 10 seconds, %s knows %s will want a comb, some soap, and a mirror." -- Requires localization
     L["Auto Res Key"] = "Touche d'auto Res"
     L["Chat Output"] = "Chat sortie"
     L["Chat output options"] = "Sortie des options de chat"
@@ -14,109 +14,109 @@ if L then
     L["Class Colours"] = "Coleurs des classes"
     L["Credits"] = "Cr\195\169dits"
     L["% doesn't think %s is immortal, but after this res cast, it is close enough."] = "% ne pensent pas que %s est immortel, mais apr\195\168s avoir jet\195\169 un sort de Res, il est assez proche." -- Needs review
-    -- L["% doesn't want the Lich King to get another soldier, so is bringing %s back to life."] = "% doesn't want the Lich King to get another soldier, so is bringing %s back to life."
+    L["% doesn't want the Lich King to get another soldier, so is bringing %s back to life."] = "% doesn't want the Lich King to get another soldier, so is bringing %s back to life." -- Requires localization
     L["Dummy"] = "Dummy"
     L["Duplicate Res Bars Colour"] = "Dupliquer les couleurs des bars de Res"
     L["Duplicate Res Targets"] = "Dupliquer la cible de Res"
     L["Everybody is alive. Congratulations!"] = "Tout le monde est en vie. F\195\169licitation !"
-    -- L["Filthy peon! %s has to resurrect %s!"] = "Filthy peon! %s has to resurrect %s!"
-    --[=[ L["For ressing targets who have not released their ghosts\
+    L["Filthy peon! %s has to resurrect %s!"] = "Filthy peon! %s has to resurrect %s!" -- Requires localization
+    L["For ressing targets who have not released their ghosts\
     Default is *"] = "For ressing targets who have not released their ghosts\
-    Default is *" ]=]
-    -- L["FOR THE HORDE! FOR THE ALLIANCE! %s thinks %s should be more concerned about yelling FOR THE LICH KING! and prevents that from happening."] = "FOR THE HORDE! FOR THE ALLIANCE! %s thinks %s should be more concerned about yelling FOR THE LICH KING! and prevents that from happening."
+    Default is *" -- Requires localization
+    L["FOR THE HORDE! FOR THE ALLIANCE! %s thinks %s should be more concerned about yelling FOR THE LICH KING! and prevents that from happening."] = "FOR THE HORDE! FOR THE ALLIANCE! %s thinks %s should be more concerned about yelling FOR THE LICH KING! and prevents that from happening." -- Requires localization
     L["Frankthetank"] = "Frankthetank"
-    --[=[ L["Gives you the pointer to click on corpses\
+    L["Gives you the pointer to click on corpses\
     Default is /"] = "Gives you the pointer to click on corpses\
-    Default is /" ]=]
-    -- L["Grow Upwards"] = "Grow Upwards"
+    Default is /" -- Requires localization
+    L["Grow Upwards"] = "Grow Upwards" -- Requires localization
     L["GUILD"] = "GUILDE"
-    -- L["% has to wake % from eternal slumber."] = "% has to wake % from eternal slumber."
-    -- L["In a world of resurrection spells, why are NPC deaths permanent? It doesn't matter, since %s is making sure %s's death isn't permanent."] = "In a world of resurrection spells, why are NPC deaths permanent? It doesn't matter, since %s is making sure %s's death isn't permanent."
-    -- L["% is bringing % back to life!"] = "% is bringing % back to life!"
-    -- L["% is ending %'s dirt nap."] = "% is ending %'s dirt nap."
+    L["% has to wake % from eternal slumber."] = "% has to wake % from eternal slumber." -- Requires localization
+    L["In a world of resurrection spells, why are NPC deaths permanent? It doesn't matter, since %s is making sure %s's death isn't permanent."] = "In a world of resurrection spells, why are NPC deaths permanent? It doesn't matter, since %s is making sure %s's death isn't permanent." -- Requires localization
+    L["% is bringing % back to life!"] = "% is bringing % back to life!" -- Requires localization
+    L["% is ending %s's dirt nap."] = "% is ending %s's dirt nap." -- Requires localization
     L["% is ressing %"] = "% ressuscite %"
-    -- L["is resurrecting "] = "is resurrecting "
+    L["is resurrecting "] = "is resurrecting " -- Requires localization
     L["I would personally like to thank Jerry on the wowace forums for coding the new, smarter, resurrection function."] = "Je tiens personnellement \195\160 remercier Jerry sur les forums de WowAce et du nouveau codage, plus intelligente, et la fonction de r\195\169surrection"
-    -- L["Just to be silly, % tickles %s until they get back up."] = "Just to be silly, % tickles %s until they get back up."
+    L["Just to be silly, % tickles %s until they get back up."] = "Just to be silly, % tickles %s until they get back up." -- Requires localization
     L["Key Bindings"] = "Raccourcis clavier"
-    -- L["Left click to lock/unlock the res bars. Right click for configuration."] = "Left click to lock/unlock the res bars. Right click for configuration."
-    -- L["Make the res bars grow up instead of down"] = "Make the res bars grow up instead of down"
+    L["Left click to lock/unlock the res bars. Right click for configuration."] = "Left click to lock/unlock the res bars. Right click for configuration." -- Requires localization
+    L["Make the res bars grow up instead of down"] = "Make the res bars grow up instead of down" -- Requires localization
     L["Manual Target Key"] = "Touche manuel de la cible"
     L["Massive kudos to Maia, Kyahx, and Poull for the original SmartRes.\
     SmartRes2 was largely possible because of\
     DathRarhek's LibResComm-1.0 so a big thanks to him."] = "Massive f\195\169licitations \195\160 Maia, Kyahx, et Poull pour l'addon original de SmartRes.\
     SmartRes2 est en grande partie possible gra\195\167e \195\160 \
     DathRarhek's et LibResComm-1.0 donc un grand merci \195\160 lui."
-    -- L["No fallen heroes! %s needs %s to march forward to victory!"] = "No fallen heroes! %s needs %s to march forward to victory!"
+    L["No fallen heroes! %s needs %s to march forward to victory!"] = "No fallen heroes! %s needs %s to march forward to victory!" -- Requires localization
     L["none"] = "Aucun"
     L["Nursenancy"] = "Nursenancy"
-    -- L["Oh. My. God. %s has to breathe life back into %s AGAIN?!?"] = "Oh. My. God. %s has to breathe life back into %s AGAIN?!?"
-    -- L["Once again, %s pulls %s and their bacon out of the fire."] = "Once again, %s pulls %s and their bacon out of the fire."
+    L["Oh. My. God. %s has to breathe life back into %s AGAIN?!?"] = "Oh. My. God. %s has to breathe life back into %s AGAIN?!?" -- Requires localization
+    L["Once again, %s pulls %s and their bacon out of the fire."] = "Once again, %s pulls %s and their bacon out of the fire." -- Requires localization
     L["Options for the res bars"] = "Options pour les bars de Res"
     L["PARTY"] = "GROUPE"
-    -- L["% performs a series of lewd acts on %'s still warm corpse. Ew."] = "% performs a series of lewd acts on %'s still warm corpse. Ew."
-    -- L["Pick the colour for collision (duplicate) res bars"] = "Pick the colour for collision (duplicate) res bars"
-    -- L["Pick the colour for non-collision (not a duplicate) res bar"] = "Pick the colour for non-collision (not a duplicate) res bar"
-    -- L["Please get SmartRes2 and use the auto res key to never see this whisper again."] = "Please get SmartRes2 and use the auto res key to never see this whisper again."
-    -- L["Prints a message to yourself whom you are ressing"] = "Prints a message to yourself whom you are ressing"
+    L["% performs a series of lewd acts on %'s still warm corpse. Ew."] = "% performs a series of lewd acts on %'s still warm corpse. Ew." -- Requires localization
+    L["Pick the colour for collision (duplicate) res bars"] = "Pick the colour for collision (duplicate) res bars" -- Requires localization
+    L["Pick the colour for non-collision (not a duplicate) res bar"] = "Pick the colour for non-collision (not a duplicate) res bar" -- Requires localization
+    L["Please get SmartRes2 and use the auto res key to never see this whisper again."] = "Please get SmartRes2 and use the auto res key to never see this whisper again." -- Requires localization
+    L["Prints a message to yourself whom you are ressing"] = "Prints a message to yourself whom you are ressing" -- Requires localization
     L["RAID"] = "RAID"
-    -- L["Random Res Messages"] = "Random Res Messages"
-    -- L["% refuses to accept blame for %s's death, but kindly undoes the damage."] = "% refuses to accept blame for %s's death, but kindly undoes the damage."
+    L["Random Res Messages"] = "Random Res Messages" -- Requires localization
+    L["% refuses to accept blame for %s's death, but kindly undoes the damage."] = "% refuses to accept blame for %s's death, but kindly undoes the damage." -- Requires localization
     L["Res Bars"] = "Barres de Res"
     L["Res Bars Anchor"] = "Res barres d'ancrage"
     L["Res Bars Background Colour"] = "Res Barres Couleur de fond"
     L["Res Bars Border"] = "Res barres de bordure"
     L["Res Bars Colour"] = "Couleurs des barres de Res"
     L["Res Bars Icon"] = "Icon des barres de Res"
-    -- L["Res Bars Scale"] = "Res Bars Scale"
+    L["Res Bars Scale"] = "Res Bars Scale" -- Requires localization
     L["Res Bars Texture"] = "Textures des barres de Res"
     L["SAY"] = "DIRE"
     L["Select the texture for the res bars"] = "S\195\169lectionnez la texture pour les barres de Res"
-    -- L["Self Notification"] = "Self Notification"
-    -- L["Set the background colour for the res bars"] = "Set the background colour for the res bars"
+    L["Self Notification"] = "Self Notification" -- Requires localization
+    L["Set the background colour for the res bars"] = "Set the background colour for the res bars" -- Requires localization
     L["Set the border for the res bars"] = "D\195\169finissez la fronti\195\168re pour les barres de Res "
     L["Set the keybindings"] = "Ensemble pour les raccourcis claviers"
-    -- L["Set the scale for the res bars"] = "Set the scale for the res bars"
-    -- L["Show or hide the icon for res spells"] = "Show or hide the icon for res spells"
-    -- L["Show the test bars"] = "Show the test bars"
-    -- L["%s knew %s couldn't stay out of the fire. *Sigh*"] = "%s knew %s couldn't stay out of the fire. *Sigh*"
-    -- L["%s knows % is faking. It was only a flesh wound!"] = "%s knows % is faking. It was only a flesh wound!"
-    -- L["%s knows that %s dying was just an excuse to see another silly random res message."] = "%s knows that %s dying was just an excuse to see another silly random res message."
-    -- L["Sleeping on the job? % is disappointed in %."] = "Sleeping on the job? % is disappointed in %."
+    L["Set the scale for the res bars"] = "Set the scale for the res bars" -- Requires localization
+    L["Show or hide the icon for res spells"] = "Show or hide the icon for res spells" -- Requires localization
+    L["Show the test bars"] = "Show the test bars" -- Requires localization
+    L["%s knew %s couldn't stay out of the fire. *Sigh*"] = "%s knew %s couldn't stay out of the fire. *Sigh*" -- Requires localization
+    L["%s knows % is faking. It was only a flesh wound!"] = "%s knows % is faking. It was only a flesh wound!" -- Requires localization
+    L["%s knows that %s dying was just an excuse to see another silly random res message."] = "%s knows that %s dying was just an excuse to see another silly random res message." -- Requires localization
+    L["Sleeping on the job? % is disappointed in %."] = "Sleeping on the job? % is disappointed in %." -- Requires localization
     L["smartres"] = "smartres" -- Needs review
     L["SmartRes2"] = "SmartRes2"
     L["SmartRes2 "] = "SmartRes2"
-    -- L["SmartRes2 Credits"] = "SmartRes2 Credits"
-    -- L["SmartRes2 would like you to know that %s is already being ressed by %s. "] = "SmartRes2 would like you to know that %s is already being ressed by %s. "
+    L["SmartRes2 Credits"] = "SmartRes2 Credits" -- Requires localization
+    L["SmartRes2 would like you to know that %s is already being ressed by %s. "] = "SmartRes2 would like you to know that %s is already being ressed by %s. " -- Requires localization
     L["Somewhere, the Lich King is laughing at %s, because he knows %s will just die again eventually. More meat for the grinder!!"] = "Quelque part, le roi-liche se moque de %s, parce qu'il sait que %s va juste mourir \195\160 nouveau. Plus de viande pour le moulin!"
-    -- L["%s prays over the corpse of %s, and a miracle happens!"] = "%s prays over the corpse of %s, and a miracle happens!"
-    -- L["%s prods %s with a stick. A-ha! % was only temporarily dead."] = "%s prods %s with a stick. A-ha! % was only temporarily dead."
+    L["%s prays over the corpse of %s, and a miracle happens!"] = "%s prays over the corpse of %s, and a miracle happens!" -- Requires localization
+    L["%s prods %s with a stick. A-ha! % was only temporarily dead."] = "%s prods %s with a stick. A-ha! % was only temporarily dead." -- Requires localization
     L["sr"] = "sr"
-    -- L["%s wonders about these stupid res messages. %s should just be happy to be alive."] = "%s wonders about these stupid res messages. %s should just be happy to be alive."
-    -- L["Test Bars"] = "Test Bars"
-    -- L["There are no bodies in range to res."] = "There are no bodies in range to res."
-    -- L["% thinks %s should work on their Dodge skill."] = "% thinks %s should work on their Dodge skill."
-    -- L["Think that was bad? % proudly shows %s the scar tissue caused by Ragnaros."] = "Think that was bad? % proudly shows %s the scar tissue caused by Ragnaros."
+    L["%s wonders about these stupid res messages. %s should just be happy to be alive."] = "%s wonders about these stupid res messages. %s should just be happy to be alive." -- Requires localization
+    L["Test Bars"] = "Test Bars" -- Requires localization
+    L["There are no bodies in range to res."] = "There are no bodies in range to res." -- Requires localization
+    L["% thinks %s should work on their Dodge skill."] = "% thinks %s should work on their Dodge skill." -- Requires localization
+    L["Think that was bad? % proudly shows %s the scar tissue caused by Ragnaros."] = "Think that was bad? % proudly shows %s the scar tissue caused by Ragnaros." -- Requires localization
     L["Timthewizard"] = "Timthewizard" -- Needs review
-    -- L["Toggles the anchor for the res bars so you can move them"] = "Toggles the anchor for the res bars so you can move them"
-    --[=[ L["Toggle whether you want to whisper a resser who is ressing a\
+    L["Toggles the anchor for the res bars so you can move them"] = "Toggles the anchor for the res bars so you can move them" -- Requires localization
+    L["Toggle whether you want to whisper a resser who is ressing a\
     target of another resser's spell.\
     Could get very spammy.\
     Default off"] = "Toggle whether you want to whisper a resser who is ressing a\
     target of another resser's spell.\
     Could get very spammy.\
-    Default off" ]=]
-    --[=[ L["Turn random res messages on or keep the same message.\
+    Default off" -- Requires localization
+    L["Turn random res messages on or keep the same message.\
     Default is off"] = "Turn random res messages on or keep the same message.\
-    Default is off" ]=]
-    -- L["Use class colours for the target on the res bars"] = "Use class colours for the target on the res bars"
-    --[=[ L["Where to print the res message.\
+    Default is off" -- Requires localization
+    L["Use class colours for the target on the res bars"] = "Use class colours for the target on the res bars" -- Requires localization
+    L["Where to print the res message.\
     Raid, Party, Say, Yell, Guild, or None.\
     Default is None"] = "Where to print the res message.\
     Raid, Party, Say, Yell, Guild, or None.\
-    Default is None" ]=]
+    Default is None" -- Requires localization
     L["YELL"] = "CRI"
-    -- L["You are ressing %s"] = "You are ressing %s"
+    L["You are ressing %s"] = "You are ressing %s" -- Requires localization
     L["You cannot cast res spells."] = "Vous ne pouvez pas lancer des sorts pour Res."
     L["You don't have enough Mana to cast a res spell."] = "Vous n'avez pas assez de mana pour lancer un sort de Res."
 end
