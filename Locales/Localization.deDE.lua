feb52e856dd2a7e1165e015df83f57dc8797ac6b
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2009-12-20 08:18:42 +0000

    added deDE, updated enUS and frFR. Fixed tickets 6, 7, (and hopefully) 8. still throwing a db error, probably because profiles are coded wrong

---
+++ Locales/Localization.deDE.lua
@@ -0,0 +1,139 @@
+﻿-- Authors: Farook, Black_Mystics
+-- Localization file for German version of SmartRes2
+
+local debug = false
+--[===[@debug@
+debug = true
+--@end-debug@]===]
+
+local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "deDE", false, debug) -- this is not the default language
+
+if L then
+    L["About"] = "\195\156ber"
+    L["About the author and SmartRes2"] = "\195\156ber den Autor und SmartRes2"
+    L["All dead units are being ressed."] = "Alle toten Spieler werden bereits wiederbelebt."
+    L["Alter the profiles"] = "Profile \195\164ndern"
+    L["And you thought the Scourge looked bad. In about 10 seconds, %s knows %s will want a comb, some soap, and a mirror."] = "And you thought the Scourge looked bad. In about 10 seconds, %s knows %s will want a comb, some soap, and a mirror." -- Requires localization
+    L["Auto Res Key"] = "Automatische Wiederbelebungstaste"
+    L["Change the horizontal direction of the res bars. Default is right to left"] = "Die horizontale Ausrichtung der Leisten \195\164ndern. Vorgabe ist von rechts nach links."
+    L["Chat Output"] = "Chat-Ausgabe"
+    L["Chat output options"] = "Chat-Ausgabe-Optionen"
+    L["Chat Output Type"] = "Art der Chat-Ausgabe"
+    L["Class Colours"] = "Klassenfarben"
+    L["Copy one profile to another"] = "Ein Profil auf ein anderes kopieren"
+    L["Copy Profile"] = "Profil kopieren"
+    L["Create a new profile"] = "Ein neues Profil erstellen"
+    L["Credits"] = "Credits"
+    L["Delete a profile you no longer use. This cannot be the active profile"] = "Ein Profil l\195\182schen welches nicht mehr verwendet wird. Dies kann nicht das aktive Profil sein."
+    L["Delete Profile"] = "Profil l\195\182schen"
+    L["% doesn't think %s is immortal, but after this res cast, it is close enough."] = "% doesn't think %s is immortal, but after this res cast, it is close enough." -- Requires localization
+    L["% doesn't want the Lich King to get another soldier, so is bringing %s back to life."] = "% m\195\182chte dem Lich-K\195\182nig keinen weitern Soldaten \195\188berlassen, somit ruft er % wieder zur\195\188ck ins Leben. " -- Needs review
+    L["Dummy"] = "Dummy" -- Requires localization
+    L["Duplicate Res Bars Colour"] = "Farbe der Wiederbelebungsbalken duplizieren" -- Needs review
+    L["Duplicate Res Targets"] = "Wiederbelebungsziele duplizieren" -- Needs review
+    L["Everybody is alive. Congratulations!"] = "Alle sind am Leben. Gratulation!"
+    L["Filthy peon! %s has to resurrect %s!"] = "Filthy peon! %s has to resurrect %s!" -- Requires localization
+    L["For ressing targets who have not released their ghosts\
+    Default is *"] = "Zum Wiederbeleben von Zielen, die ihren Geist nicht freigelassen haben.\
+    Vorgabe ist *"
+    L["FOR THE HORDE! FOR THE ALLIANCE! %s thinks %s should be more concerned about yelling FOR THE LICH KING! and prevents that from happening."] = "FOR THE HORDE! FOR THE ALLIANCE! %s thinks %s should be more concerned about yelling FOR THE LICH KING! and prevents that from happening." -- Requires localization
+    L["Frankthetank"] = "Frankthetank" -- Requires localization
+    L["Gives you the pointer to click on corpses\
+    Default is ~"] = "L\195\164sst den Zeiger erscheinen um auf Leichen direkt zu klicken.\
+    Vorgabe ist ~"
+    L["Grow Upwards"] = "Nach oben ansteigen"
+    L["GUILD"] = "GILDE"
+    L["% has to wake % from eternal slumber."] = "% has to wake % from eternal slumber." -- Requires localization
+    L["Horizontal Direction"] = "Horizontale Ausrichtung"
+    L["In a world of resurrection spells, why are NPC deaths permanent? It doesn't matter, since %s is making sure %s's death isn't permanent."] = "In a world of resurrection spells, why are NPC deaths permanent? It doesn't matter, since %s is making sure %s's death isn't permanent." -- Requires localization
+    L["% is bringing % back to life!"] = "% ruft % zur\195\188ck ins Leben!" -- Needs review
+    L["% is ending %s's dirt nap."] = "% is ending %s's dirt nap." -- Requires localization
+    L["% is ressing %"] = "% belebt % wieder" -- Needs review
+    L["is resurrecting "] = "wirkt Wiederbelebung"
+    L["I would personally like to thank Jerry on the wowace forums for coding the new, smarter, resurrection function."] = "Ich m\195\182chte mich pers\195\182nlich bei Jerry aus dem WowAce-Forum f\195\188r das Entwickeln einer neuen, kl\195\188geren Wiederbelebungsfunktion bedanken."
+    L["Just to be silly, % tickles %s until they get back up."] = "Just to be silly, % tickles %s until they get back up." -- Requires localization
+    L["Key Bindings"] = "Tastenbelegung"
+    L["Left click to lock/unlock the res bars. Right click for configuration."] = "Linksklick um die Wiederbelebungsleisten zu sperren/entsperren. Rechtsklick zum Konfigurieren."
+    L["Make the res bars grow up instead of down"] = "Die Leisten nach oben ansteigen lassen"
+    L["Manual Target Key"] = "Manuelle Zieltaste"
+    L["Massive kudos to Maia, Kyahx, and Poull for the original SmartRes.\
+    SmartRes2 was largely possible because of DathRarhek's LibResComm-1.0 so a big thanks to him."] = "Ein gro\195\159es Dankesch\195\182n an Maia, Kyahx and Poull f\195\188r das urspr\195\188ngliche SmartRes.\
+    SmartRes2 war zum gr\195\182\195\159ten Teil m\195\182glich aufgrund von LibResComm-1.0 von DathRarhek (deshalb auch ein Dankesch\195\182n an ihn)."
+    L["New Profile"] = "Neues Profil"
+    L["No fallen heroes! %s needs %s to march forward to victory!"] = "No fallen heroes! %s needs %s to march forward to victory!" -- Requires localization
+    L["none"] = "nichts"
+    L["Nursenancy"] = "Nursenancy" -- Requires localization
+    L["Oh. My. God. %s has to breathe life back into %s AGAIN?!?"] = "Oh. My. God. %s has to breathe life back into %s AGAIN?!?" -- Requires localization
+    L["Once again, %s pulls %s and their bacon out of the fire."] = "Once again, %s pulls %s and their bacon out of the fire." -- Requires localization
+    L["Options for the res bars"] = "Optionen f\195\188r die Wiederbelebungsleisten"
+    L["PARTY"] = "GRUPPE"
+    L["% performs a series of lewd acts on %'s still warm corpse. Ew."] = "% performs a series of lewd acts on %'s still warm corpse. Ew." -- Requires localization
+    L["Pick the colour for collision (duplicate) res bars"] = "Farbe f\195\188r kollidierende/doppelte Wiederbelebungen ausw\195\164hlen"
+    L["Pick the colour for non-collision (not a duplicate) res bar"] = "Farbe f\195\188r nicht kollidierende/nicht doppelte Wiederbelebungen ausw\195\164hlen"
+    L["Please get SmartRes2 and use the auto res key to never see this whisper again."] = "Lege dir SmartRes2 zu, verwende die Auto-Wiederbelebungstaste, und du wirst diese Nachricht nie wieder sehen."
+    L["Prints a message to yourself whom you are ressing"] = "Sendet dir selbst eine Nachricht wen du gerade wiederbelebst. "
+    L["Profiles"] = "Profile"
+    L["RAID"] = "SCHLACHTZUG"
+    L["Random Res Messages"] = "Zuf\195\164llige Wiederbelebungsnachrichten "
+    L["% refuses to accept blame for %s's death, but kindly undoes the damage."] = "% refuses to accept blame for %s's death, but kindly undoes the damage." -- Requires localization
+    L["Res Bars"] = "Wiederbelebungsleisten"
+    L["Res Bars Anchor"] = "Ankerpunkt"
+    L["Res Bars Background Colour"] = "Hintergrundfarbe der Wiederbelebungsleisten"
+    L["Res Bars Border"] = "Rahmen der Wiederbelebungsleisten"
+    L["Res Bars Colour"] = "Farbe der Wiederbelebungsleisten"
+    L["Res Bars Icon"] = "Symbol der Wiederbelebungsleisten"
+    L["Res Bars Scale"] = "Skalierung der Wiederbelebungsleisten"
+    L["Res Bars Texture"] = "Textur der Wiederbelebungsleisten"
+    L["Reset a profile back to defaults"] = "Ein Profil auf die Ausgangswerte zur\195\188cksetzen"
+    L["Reset Profile"] = "Profil zur\195\188cksetzen"
+    L["SAY"] = "SAGEN"
+    L["Select the texture for the res bars"] = "Die Textur f\195\188r die Wiederbelebungsleisten ausw\195\164hlen"
+    L["Self Notification"] = "Selbstbenachrichtigung"
+    L["Set the background colour for the res bars"] = "Die Hintergrundfarbe f\195\188r die Wiederbelebungsleisten ausw\195\164hlen"
+    L["Set the border for the res bars"] = "Den Rahmen f\195\188r die Wiederbelebungsleisten ausw\195\164hlen"
+    L["Set the keybindings"] = "Tastenbelegungen festlegen"
+    L["Set the scale for the res bars"] = "Stellt die Gr\195\182\195\159e der Wiederbelebungsleisten ein"
+    L["Show or hide the icon for res spells"] = "Anzeigen oder Verstecken der Symbole f\195\188r Wiederbelebungszauber"
+    L["Show the test bars"] = "Zeige die Test-Leisten"
+    L["%s knew %s couldn't stay out of the fire. *Sigh*"] = "%s knew %s couldn't stay out of the fire. *Sigh*" -- Requires localization
+    L["%s knows % is faking. It was only a flesh wound!"] = "% wei\195\159 das % nur simuliert. Es war nur eine Fleischwunde!" -- Needs review
+    L["%s knows that %s dying was just an excuse to see another silly random res message."] = "% wei\195\159 das %s Tod nur ein Vorwand war um eine andere bescheuerte Zufallswiederbelebungsnachricht zu sehen. " -- Needs review
+    L["Sleeping on the job? % is disappointed in %."] = "Sleeping on the job? % is disappointed in %." -- Requires localization
+    L["smartres"] = "smartres"
+    L["SmartRes2"] = "SmartRes2"
+    L["SmartRes2 "] = "SmartRes2"
+    L["SmartRes2 Credits"] = "SmartRes2 Credits"
+    L["SmartRes2 would like you to know that %s is already being ressed by %s. "] = "SmartRes2 m\195\182chte dir mitteilen, dass %s bereits von %s wiederbelebt wird."
+    L["Somewhere, the Lich King is laughing at %s, because he knows %s will just die again eventually. More meat for the grinder!!"] = "Somewhere, the Lich King is laughing at %s, because he knows %s will just die again eventually. More meat for the grinder!!" -- Requires localization
+    L["%s prays over the corpse of %s, and a miracle happens!"] = "%s prays over the corpse of %s, and a miracle happens!" -- Requires localization
+    L["%s prods %s with a stick. A-ha! % was only temporarily dead."] = "%s prods %s with a stick. A-ha! % was only temporarily dead." -- Requires localization
+    L["sr"] = "sr"
+    L["%s wonders about these stupid res messages. %s should just be happy to be alive."] = "% wundert sich \195\188ber die bl\195\182den Zufallsnachrichten. % sollte froh sein am Leben zu sein. " -- Needs review
+    L["Test Bars"] = "Test-Leisten"
+    L["There are no bodies in range to res."] = "Es gibt in der N\195\164he keine Spieler zum Wiederbeleben."
+    L["% thinks %s should work on their Dodge skill."] = "% denkt das % an seiner Ausweichtechnik arbeiten muss." -- Needs review
+    L["Think that was bad? % proudly shows %s the scar tissue caused by Ragnaros."] = "Think that was bad? % proudly shows %s the scar tissue caused by Ragnaros." -- Requires localization
+    L["Timthewizard"] = "Timthewizard" -- Requires localization
+    L["Toggles the anchor for the res bars so you can move them"] = "Schaltet den Anker f\195\188r die Wiederbelebungsleisten ein/aus, um die Leistenposition(en) zu \195\164ndern"
+    L["Toggle whether you want to whisper a resser who is ressing a\
+    target of another resser's spell.\
+    Could get very spammy.\
+    Default off"] = "Ein/Ausschalten ob du andere Spieler anwhispern m\195\182chtest, wenn sie jemanden \
+    wiederbeleben, der bereits von einer anderen Person wiederbelebt wird.\
+    K\195\182nnte in Spam ausarten.\
+    Vorgabe ist \"Aus\"." -- Needs review
+    L["Turn random res messages on or keep the same message.\
+    Default is off"] = "Zuf\195\164llige Wiederbelebungsnachrichten einschalten oder die selbe Nachricht behalten.\
+    Vorgabe ist \"Aus\"."
+    L["Use class colours for the target on the res bars"] = "Benutze die Klassenfarbe des Ziels auf der Wiederbelebungsleiste."
+    L["Where to print the res message.\
+    Raid, Party, Say, Yell, Guild, or None.\
+    Default is None"] = "Wo die Wiederbelebungsnachricht angezeigt werden soll.\
+    Schlachtzug, Gruppe, Sagen, Schreien, Gilde oder Keine Ausgabe.\
+    Standard ist \"Keine Ausgabe\"."
+    L["YELL"] = "SCHREIEN"
+    L["You are not in a group."] = "Du bist in keiner Gruppe."
+    L["You are ressing %s"] = "Du belebst %s wieder"
+    L["You cannot cast res spells."] = "Du kannst keine Wiederbelebungszauber wirken. "
+    L["You don't have enough Mana to cast a res spell."] = "Du hast nicht gen\195\188gend Mana um einen Wiederbelebungszauber zu wirken."
+end
