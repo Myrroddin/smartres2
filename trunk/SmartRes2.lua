24095ab30c7c3b8bb289f495c03f1c22bc54e59f
shefki <shefki@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2010-01-12 22:54:45 +0000

    Fix the patterns for replacing the player names in the chat announcements.

--- trunk/SmartRes2.lua
+++ trunk/SmartRes2.lua
@@ -697,8 +697,8 @@ function SmartRes2:ResComm_ResStart(event, sender, endTime, targetName)
 			if self.db.profile.randMsgs then
 				msg = math.random(#self.db.profile.randChatTbl)
 			end
-			sgsub(msg, "%%p%%", sender)
-			sgsub(msg, "%%t%%", targetName)
+			sgsub(msg, "%%%%p%%%%", sender)
+			sgsub(msg, "%%%%t%%%%", targetName)
 			SendChatMessage(msg, channel, nil, nil)
 		end
 		if self.db.profile.notifySelf then
