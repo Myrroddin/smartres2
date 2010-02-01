4d482c584b80e7b68cf405a74a8ae3bd1592b917
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2010-02-01 11:29:15 +0000

    zhTW added custom chat message support added fixed some localization bugs should finally check, and skip, units already being ressed

---
+++ trunk/Locales/Localization.zhTW.lua
@@ -0,0 +1,10 @@
+﻿-- Authors: Whocare
+-- Localization file for Traditional Chinese version of SmartRes2
+
+local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "zhTW")
+if not L then return end
+
+
+--[===[@non-debug@
+@localization(locale="zhTW", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@end-non-debug@]===]
