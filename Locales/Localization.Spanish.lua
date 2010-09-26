c2589c039a74b437f7703a2af47bfcd8c8b1d478
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2010-09-26 04:35:38 +0000

    .pkgmeta: fixed tabs vs spaces, removed tag: latest for all libs Rolled esMX into the more complete esES locale Some small nerfs to the version checking to decrease obnoxiousness

---
+++ Locales/Localization.Spanish.lua
@@ -0,0 +1,8 @@
+-- Authors: Silmano
+-- Localization file for Spanish version of SmartRes2
+
+local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "esMX") or LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "esES")
+if L then
+	--@localization(locale="esES", format="lua_additive_table", table-name="L", same-key-is-true=true, handle-unlocalized="english")@
+	return
+end
