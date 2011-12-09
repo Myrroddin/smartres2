436523f793516eaa2e8451d8d63b9ecf6f11c319
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2011-12-09 21:16:39 +0000

    - Add Brazilian Portuguese file - Remove all references to making res bars flash, because that never worked correctly - Made the slash command cast developer versions only until fix is found - Used Blizzard's global strings wherever possible

---
+++ Locales/Localization.ptBR.lua
@@ -0,0 +1,9 @@
+-- Localization file for Brazillian Portuguese version of SmartRes2
+
+local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "ptBR")
+if not L then return end
+
+
+--[===[@non-debug@
+@localization(locale="ptBR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@end-non-debug@]===]
