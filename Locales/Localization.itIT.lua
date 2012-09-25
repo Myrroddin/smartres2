bdf0f9cc8e282ccd6a4a9a9b9dbc17656884c1a6
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2012-09-25 03:16:17 +0000

    - Fix all the leaking global variables thanks to Sonayahh for the bug report

--- Locales/Localization.itIT.lua
+++ Locales/Localization.itIT.lua
@@ -1,6 +1,8 @@
---@file-author@
---@file-revision@
---@file-date-iso@
+--[[
+File author: --@file-author@
+File revision: --@file-revision@
+Date updated: --@file-date-iso@
+]]--
 
 local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "itIT")
 if not L then return end
