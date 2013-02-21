4f3d057b83e62d94a9a04bf56df4ddc03bbb552b
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2013-02-21 07:35:25 +0000

    --- TESTING BUILD --- - Switch to LibResInfo from LibResComm - Some changes to options (you won't lose random messages!) - Update localizations (more changes coming) - Various major and minor changes behind the scenes

--- Locales/Localization.ptBR.lua
+++ Locales/Localization.ptBR.lua
@@ -7,4 +7,4 @@ Date updated: --@file-date-iso@
 local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "ptBR")
 if not L then return end
 
---@localization(locale="ptBR", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@localization(locale="ptBR", format="lua_additive_table")@
--- Locales/Localization.zhCN.lua
+++ Locales/Localization.zhCN.lua
@@ -7,4 +7,4 @@ Date updated: --@file-date-iso@
 local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "zhCN")
 if not L then return end
 
---@localization(locale="zhCN", format="lua_additive_table", same-key-is-true=true, handle-unlocalized="english")@
+--@localization(locale="zhCN", format="lua_additive_table")@
