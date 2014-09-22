7384bae60a57ea45bcc15ff1ff61b4c5bf5adf47
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2014-09-22 05:21:14 +0000

    - WoD working copy

---
+++ trunk/WoD/Locales/Localization.enUS.lua
@@ -0,0 +1,14 @@
+--[[
+File author: --@file-author@
+File revision: --@file-revision@
+Date updated: --@file-date-iso@
+]]--
+      
+local debug = false
+--@debug@
+debug = true
+--@end-debug@
+
+local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "enUS", true, debug)
+
+--@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true)@
