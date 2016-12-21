237b52b826cd6fe56faa0a087fb3bd7b305f8444
myrroddin <myrroddin@5973fdcc-91e6-4ec6-beb5-091bfcfbd39b>
2016-12-21 04:25:52 +0000

    - Add tools-used for FindGlobals (thanks, Mikk!) - More phrases added to localization, and ordered them alphabetically in the enUS file - Modules can take care of their own options within namespaces, thank you very much

--- trunk/Locales/Localization.enUS.lua
+++ trunk/Locales/Localization.enUS.lua
@@ -13,6 +13,8 @@ local L = LibStub("AceLocale-3.0"):NewLocale("SmartRes2", "enUS", true, debug)
 
 L["Lock Button"] = true
 L["Lock minimap button and prevent moving."] = true
+L["Reset Button"] = true
+L["Reset the minimap button to defaults (position, visible, locked)."] = true
 L["Show or hide the minimap icon."] = true
-L["Toggle SmartRes2 and all modules on/off."] = true
 L["Toggle module on/off."] = true
+L["Toggle SmartRes2 and all modules on/off."] = true
